e05ed1e34e8095fec63dc56cbe27306036074cc3
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2021-03-13 15:21:01 -0300

    first commit

---
+++ 3-fibonacci.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+import time
+
+
+cache = {}
+
+
+@lru_cache(maxsize=10000)
+def fibonacci(n):
+    if n in cache:
+        return cache[n]
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 2
+    elif n > 2:
+        value = fibonacci(n-1) + fibonacci(n-2)
+
+    cache[n] = value
+    return value
+
+
+start = time.time()
+
+for n in range(1, 10):
+    print(n, ":", fibonacci(n))
+
+stop = time.time()
+print("time:", stop - start)
